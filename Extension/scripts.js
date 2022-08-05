677f27a6de76d92b8047d7ea7c9c4144c03585d2
Morten Christiansen <mc@uvdata.dk>
2022-08-05 14:08:49 +0200

    Cleanup

--- Extension/scripts.js
+++ Extension/scripts.js
@@ -1,18 +1,3 @@
 window.onload = () => {
-    modifyRetrospectiveBoardStyles();
-}
-
-function modifyRetrospectiveBoardStyles() {
-    let tagsContainers = document.querySelectorAll('.external-content--iframe');
-    if (tagsContainers.length > 0) {
-        tagsContainers.forEach(c => {
-            let htmlELement = c.contentWindow.document.querySelectorAll('html');
-            console.log("HTML", htmlELement);
-        });
-        return;
-    }
-
-    setTimeout(() => {
-        modifyRetrospectiveBoardStyles();
-    }, 1000);
+    
 }
